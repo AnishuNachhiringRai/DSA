2b4bfdd75029801e7133accec912f3b254cb5f24
AnishuNachhiringRai <91244247+AnishuNachhiringRai@users.noreply.github.com>
2022-08-14 17:02:25 +0545

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<string.h>
+int precedency(char);
+void 
+main()
+{
+	int i,otos=-1,ptos=-1,l, l1;
+	char infix[100],poststack[100],opstack[100];
+	printf("Enter a valid infix\n");
+	gets(infix);
+	l=strlen(infix);
+	l1=l;
+	for(i=0;i<=l-1;i++)
+	{
+		if(infix[i]=='(')
+		{
+			opstack[++otos]=infix[i];
+			l1++;
+		}
+		else if(isalpha(infix[i]))
+		{
+			poststack[++ptos]=infix[i];
+		}
+		else if (infix[i]==')')
+		{
+			l1++;
+			while(opstack[otos]!='(')
+			{	
+				poststack[++ptos]=opstack[otos];
+				otos--;
+			}
+			otos--;
+		}
+		else
+		{
+			if(precedency(opstack[otos])>precedency(infix[i]))
+			{
+				poststack[++ptos]=opstack[otos--];
+				opstack[++otos]=infix[i];
+			}
+			opstack[++otos]=infix[i];
+		}
+	}
+	while(otos!=-1)
+	{
+		poststack[++ptos]=opstack[otos];
+		otos--;
+	}
+	for(i=0;i<l1;i++)
+	{
+		printf("%c",poststack[i]);
+	}
+	getch();
+	}
+	int precedency(char ch)
+	{
+		switch(ch)
+		{
+		case '$':
+			return(4);
+		case'*':
+		case'/':
+		return(3);
+		case'+':
+		case'-':
+		return(2);
+		default:
+		return(1);
+		}
+	}
